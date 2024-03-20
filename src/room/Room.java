32dce3ea710ba8575a5e60038ebedaf41716e533
Pallos Gábor György <pallosgabor99@gmail.com>
2024-03-20 16:03:28 +0100

    split, merge

--- src/room/Room.java
+++ src/room/Room.java
@@ -6,7 +6,10 @@ import player.Player;
 import player.Professor;
 import player.Student;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -82,28 +85,70 @@ public class Room {
     /**
      *
      */
-    void poisonPlayers() {
+    public void poisonPlayers() {
         players.forEach(p -> p.poison());
     }
 
-    /**
-     *
-     */
-    void split() {
 
+    /**
+     * Kettéosztja ezt a szobát.
+     * Az újonnan létrehozott szobák megosztoznak az eredeti szoba tulajdonságain.
+     * A szoba szomszédai mostantól az új szobáknak csak egyikével lesznek szomszédosak.
+     * A szobának legalább 4 kapacitásúnak kell lennie a szétosztáshoz.
+     * Az újonnan létrehozott szobák egymással szomszédosak lesznek.
+     */
+    public void split() {
+        if (capacity < 4) return;
+        Room room = new Room();
+        
+        for (int i = 0; i < doors.size(); i += 2) {
+            room.doors.add(doors.remove(i));
+        }
+        
+        for (int i = 0; i < items.size(); i += 2) {
+            room.items.add(items.remove(i));
+        }
+
+        for (int i = 0; i < effects.size(); i += 2) {
+            room.effects.add(effects.remove(i));
+        }
+
+        Door door = new Door(this, room, true, true);
+        doors.add(door);
+        room.doors.add(door);
     }
 
     /**
-     * @param room
+     * Beleolvasztja akapott szobát ebbe a szobába.
+     * A kapacitás a két szoba kapacitásának maximuma lesz.
+     * A játékosok, tárgyak és hatások összeadódnak.
+     * Az ajtók összegyűjtésekor azokat az ajtókat eldobjuk, amik a két szoba között vannak.
+     * Az ajtók közül azokat, amik a kapott szobához vezetnek, átállítjuk, hogy erre a szobára vezessenek.
+     * @param room a szoba amit beleolvasztunk ebbe a szobába
      */
-    void mergeWithRoom(Room room) {
+    public void mergeWithRoom(Room room) {
+        this.capacity = Math.max(this.capacity, room.capacity); 
+        this.players.addAll(room.players);
+        this.items.addAll(room.items);
+        this.effects.addAll(room.effects);
 
+        doors.addAll(room.doors);
+        doors = doors.stream()
+                        .filter(d -> !(d.getRoom1() == room && d.getRoom2() == this  
+                                    || d.getRoom1() == this && d.getRoom2() == room))
+                        .distinct()
+                        .collect(Collectors.toList());
+        doors.forEach(d -> {
+            if(d.getRoom1() == room) d.setRoom1(this);
+            if(d.getRoom2() == room) d.setRoom2(this);
+        });
     }
 
     /**
      * @param player
      */
     void addPlayer(Player player) {
+        this.players.add(player);
 
     }
 
@@ -111,6 +156,7 @@ public class Room {
      * @param player
      */
     void removePlayer(Player player) {
+        this.players.remove(player);
 
     }
 
@@ -118,13 +164,13 @@ public class Room {
      * @param effect
      */
     void addEffect(RoomEffect effect) {
-
+        this.effects.add(effect);
     }
 
     /**
      * @param effect
      */
     void removeEffect(RoomEffect effect) {
-
+        this.effects.remove(effect);
     }
 }
