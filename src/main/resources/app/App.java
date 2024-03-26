917248bb424d89bfd346aecb9d204c1fef61f5c8
jajecnikmarci <63594328+jajecnikmarci@users.noreply.github.com>
2024-03-26 23:05:08 +0100

    Delete src/main/resources directory

--- src/main/resources/app/App.java
+++
@@ -1,9 +0,0 @@
-package main.resources.app;
-import main.resources.skeleton.Skeleton;
-
-public class App {
-    public static Skeleton skeleton = new Skeleton();
-    public static void main(String[] args) {
-        skeleton.menu();
-    }   
-}   
