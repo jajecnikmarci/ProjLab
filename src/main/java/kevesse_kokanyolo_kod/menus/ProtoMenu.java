07f582fa5efc0bc7b34c22c777f16a349acc106c
Pallos Gábor György <pallosgabor99@gmail.com>
2024-04-05 21:15:18 +0200

    proto nyelv finomítás

--- src/main/java/kevesse_kokanyolo_kod/menus/ProtoMenu.java
+++ src/main/java/kevesse_kokanyolo_kod/menus/ProtoMenu.java
@@ -22,29 +22,46 @@ public class ProtoMenu {
         printer = new Printer(inputFileName, outputFileName);
     }
 
-    // minden objektum kap egy nevet: instanceName
+    // CONFIG:
+    // randomness <enable|disable> - véletlenszerűség be- vagy kikapcsolása, 
+    //   bármi ami véletlenszerű viselkedést kér, a ProtoMenüvel kiválasztatja az eredményt, ha a randomness ki van kapcsolva.
+    // timercontrol <enable|disable> - Ha be van kapcsolva, a felhasználó átveszi az időzítők vezérlését a programtól, ezzel aktiválja a timer parancsot.   
+    // load <CONFIG_FILE_PATH> - konfigurációs fájl betöltése (a proto nyelvén írt)
+    // printstate <InstanceName> - A megadott objektum állapotának kiírása 
+    // printall - Az összes objektum állapotának kiírása
+
     // I. INICIALIZÁLÁS: minden teszteset ezzel indul, ez egy olyan valós állapot, amiből a játék kiindulhat.
-    // pálya létrehozása: 
-    // 1. Szobák létrehozása
-    //  1.1 Tárgyak hozzáadása a szobákhoz.
-    // 2. Ajtók létrehozása
-    // 3. Személyek létrehozása, szobához adása.
-    // 4. endinit: printState(Printer) minden osztályra
+    // pálya létrehozása. 
+    // Minden objektum kap egy nevet: instanceName
+    // Ajánlott inicializálási sorrend: 
+    // 1. Szobák létrehozása (Adja meg a szoba nevét: <RoomName> )
+    // add room <RoomName>
+
+    // 2. Tárgyak hozzáadása a szobákhoz. (Adjon tárgyt a szobához: <RoomName> <ItemType> <ItemName>)
+    // add item <RoomName> <ItemType> <ItemName> 
+
+    // 3. Ajtók létrehozása (Adjon hozzá ajtókat a szobához: [!]<RoomName> <RoomName> [?cursed] Tegyen felkiáltójelet az 1. szoba neve elé, ha nem átjárható abból az irányból. A cursed opcionális true/false értéket lehet megadni, default = false.) 
+    // add door [!]<RoomName> <RoomName>  [?cursed]
+
+    // 4. Személyek létrehozása, szobához adása. (Adjon hozzá személyeket a szobához: <RoomName> <PersonType> <PersonName>) 
+    // add person <RoomName> <PersonType> <PersonName>
+
     // II. CONTROL
     // Parancsok:
-    // pickup <Academic>
-    // drop <Academic> <Item>
-    // use <Academic> <Item>
-    // gotoroom <Person> <Room>
-    // split <Room>
-    // merge <Room> <Room>
+    // pickup <Academic> - tárgy felvétele a szobából
+    // drop <Academic> <Item> - tárgy eldobása
+    // use <Academic> <Item> - tárgy használata
+    // gotoroom <Person> <Room> - átmegy egy személy egyik szobából a másikba
+    // shake - Tér-Idő rengést hajt végre
+    // endtimer <TimerName> - Leállítja az időzítőt
+    // Ctrl+C - kilépés
 
 
     // Megjegyzések: 
-    // A <> közötti szavak példányneveket jelölnek.
-    // Academic: egy hallgató, vagy oktató példány neve
-    // Person: egy hallgató, oktató, takarító példány neve
-    // A proto nyelv értelmezője hibát dob ha helytelen a parancs.
-    // Új példány létrehozása:
-    // new <ClassName> <instanceName> {Door, Room, Student, Professor, Cleaner, Items, FakeItems}
+    // - A <> közötti szavak kötelező paramétereket jelölnek.
+    // - A [] közötti szavak opcionális paramétereket jelölnek.
+    // - Ha a timercontrol be van kapcsolva, az időzítők indulásukkor kiírják a nevüket a felhasználónak, és a felhasználó beírhatja az endtimer <TimerName> parancsot, hogy leállítsa az időzítőt.
+    // - Academic: egy hallgató, vagy oktató példány neve
+    // - Person: egy hallgató, oktató, takarító példány neve
+    // - A proto nyelv értelmezője hibát dob ha helytelen a parancs.
 }
