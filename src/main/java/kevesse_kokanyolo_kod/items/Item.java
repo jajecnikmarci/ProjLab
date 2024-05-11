85ec570052328e7942dd42817ff056b509f35d25
Pallos Gábor György <pallosgabor99@gmail.com>
2024-05-11 19:36:28 +0200

    stateChangedObservable implemented for Items

--- src/main/java/kevesse_kokanyolo_kod/items/Item.java
+++ src/main/java/kevesse_kokanyolo_kod/items/Item.java
@@ -1,11 +1,19 @@
 package kevesse_kokanyolo_kod.items;
 
 import kevesse_kokanyolo_kod.effects.Effect;
+import kevesse_kokanyolo_kod.observer.IStateChangedObservable;
+import kevesse_kokanyolo_kod.observer.StateChangedObservable;
+import kevesse_kokanyolo_kod.observer.StateChangedObserver;
 
 /**
  * Egy tárgyat reprezentáló osztály
  */
-public abstract class Item implements IItem {
+public abstract class Item implements IItem, IStateChangedObservable<Item> {
+
+    private StateChangedObservable<Item> stateChangedObservable;
+    Item() {
+        stateChangedObservable = new StateChangedObservable<>(this);
+    }
     /**
      * A tárgyhoz tartozó effektet tárolja, hogy meg lehessen szüntetni eldobáskor, 
      * vagy felvételkor a játékoshoz lehessen adni.
@@ -26,5 +34,17 @@ public abstract class Item implements IItem {
     @Override 
     public Effect getEffect() {
         return effect;
+    }
+
+    @Override
+    public void addObserver(StateChangedObserver<Item> observer) {
+        stateChangedObservable.addObserver(observer);
+        
+    }
+
+    @Override
+    public void notifyStateChanged() {
+        stateChangedObservable.notifyStateChanged();
+        
     }
 }
